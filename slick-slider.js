1ca5a8effae19ab1b1b20fe4aa4ab749386904cf
playfluent <joe@viasana.co>
2023-12-29 16:09:16 -0600

    style updates

--- slick-slider.js
+++ slick-slider.js
@@ -1,3 +1,4 @@
+// Testimonial Slider
 $('.testimonial-container').slick({
     dots: true,
     infinite: true,
@@ -11,17 +12,38 @@ $('.testimonial-container').slick({
   }
 );
 
+// Retreat Gallery
 $('.retreat-gallery').slick({
   asNavFor: '.slider-nav',
   arrows: false,
   fade: true,
 });
 $('.slider-nav').slick({
-  slidesToShow: 6,
+  slidesToShow: 5,
   slidesToScroll: 1,
   asNavFor: '.retreat-gallery',
   centerMode: true,
   focusOnSelect: true,
   autoplay: true,
   autoplaySpeed: 3000,
+  responsive: [
+    {
+      // MD Break
+      breakpoint: 768,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        slidesToShow: 3
+      }
+    },
+    {
+      // SM Break
+      breakpoint: 640,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        slidesToShow: 2,
+      }
+    }
+  ]
 });
