12b1c780be6b62bad67dea3615a58ac28bc7308a
playfluent <joe@viasana.co>
2024-01-27 11:20:23 -0800

    fix/tag filtering
    * feature works now on blog-ready

--- tagFilter.js
+++ tagFilter.js
@@ -1,27 +1,5 @@
-// // Function to filter posts by tag
-// function filterPostsByTag(selectedTag) {
-//   const allPosts = document.querySelectorAll('.post-item');
-//   allPosts.forEach(post => {
-//     const postTags = post.getAttribute('data-tags').split(',');
-//     if (selectedTag === 'All' || postTags.includes(selectedTag)) {
-//       post.style.display = '';
-//     } else {
-//       post.style.display = 'none';
-//     }
-//   });
-// }
-
-// // Event listeners for tag options
-// document.querySelectorAll('.tag-option').forEach(tagOption => {
-//   tagOption.addEventListener('click', function(event) {
-//     event.preventDefault();
-//     const selectedTag = this.getAttribute('data-tag');
-//     filterPostsByTag(selectedTag);
-//   });
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
-  const tags = document.querySelectorAll('.tag-selection');
+  const tags = document.querySelectorAll('.tag-option');
 
   tags.forEach(tag => {
     tag.addEventListener('click', function(event) {
